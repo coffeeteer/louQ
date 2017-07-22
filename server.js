900fa6d64ecbc9aa757bf04445b1c703c8d0e83a
Coffeeteer <tgrieshiemer@gmail.com>
2017-07-21 21:38:40 -0400

    making the way for handlebars templating engine

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const http = require('http');
+const hbs = require('express-handlebars')
 
 const app = express();
 
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-	res.sendFile('index.html');
+// app.get('/', (req, res) => {
+// 	res.sendFile('index.html');
+// });
+
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 const server = new http.Server(app);
