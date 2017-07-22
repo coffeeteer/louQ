d0ead94aa272820b657aee4d496bf616af77f135
Coffeeteer <tgrieshiemer@gmail.com>
2017-07-21 23:15:04 -0400

    filled out header and footer information

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('home');
 });
 
 const server = new http.Server(app);
