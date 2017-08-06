8ab9db69ee798ae9a387de6f3e7219c2330c3fa8
Travis <tgrieshiemer@gmail.com>
2017-08-06 02:50:37 +0000

    made more adjustments to the top header

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
 	console.log('i haz page load');
 	// alert('I haz page load');
-	$('.carousel').carousel({
+	$('#carousel').carousel({
 	  interval: 2000
 	});
 
 	//Gallery Carousel Beginning **********************
-	$('#myCarousel').carousel({
-                interval: 5000
-        });
+// 	$('#myCarousel').carousel({
+//                 interval: 5000
+//         });
  
-        //Handles the carousel thumbnails
-        $('[id^=carousel-selector-]').click(function () {
-        var id_selector = $(this).attr("id");
-        try {
-            var id = /-(\d+)$/.exec(id_selector)[1];
-            console.log(id_selector, id);
-            jQuery('#myCarousel').carousel(parseInt(id));
-        } catch (e) {
-            console.log('Regex failed!', e);
-        }
-    });
-    // When the carousel slides, auto update the text
-    $('#myCarousel').on('slid.bs.carousel', function (e) {
-             var id = $('.item.active').data('slide-number');
-            $('#carousel-text').html($('#slide-content-'+id).html());
-    });
+//         //Handles the carousel thumbnails
+//         $('[id^=carousel-selector-]').click(function () {
+//         var id_selector = $(this).attr("id");
+//         try {
+//             var id = /-(\d+)$/.exec(id_selector)[1];
+//             console.log(id_selector, id);
+//             jQuery('#myCarousel').carousel(parseInt(id));
+//         } catch (e) {
+//             console.log('Regex failed!', e);
+//         }
+//     });
+//     // When the carousel slides, auto update the text
+//     $('#myCarousel').on('slid.bs.carousel', function (e) {
+//              var id = $('.item.active').data('slide-number');
+//             $('#carousel-text').html($('#slide-content-'+id).html());
+//     });
 
     //Contact Us Modal
     // $('#lousContactUs').modal({backdrop: 'true'});
