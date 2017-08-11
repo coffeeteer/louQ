cf5cf0ce9d7c855dd61d58410eb2b298e446b2b4
Coffeeteer <tgrieshiemer@gmail.com>
2017-08-11 14:29:00 -0400

    starting a mongoose database

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const http = require('http');
-var exphbs  = require('express-handlebars')
+const exphbs  = require('express-handlebars')
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Mongoose *************************************
+mongoose.connect('mongodb://localhost/my_database');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+	//we're connected
+});
+
+const contactForm = mongoose.Schema({
+	name: String
+});
+
+var person = mongoose.model('person', contactForm);
+
+var john = new person({name: 'John'});
+console.log(john.name);
+
+contactForm.methods.speak = function(){
+	var greeting = this.name
+	? "Hello my name is " + this.name
+	: "I forgot my name. *gives you a dumb look"
+};
+
+var person = mongoose.model('person', contactForm);
+
+var buffy = new person({name: 'Buffy'});
+buffy.speak();
+//Mongoose ***********************---------------
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 // app.set('views', path.join(__dirname, 'views'));
