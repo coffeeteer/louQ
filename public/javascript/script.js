9a62983786b925706d138748a5cbccce5ff0c93d
Coffeeteer <tgrieshiemer@gmail.com>
2017-08-10 20:09:11 -0400

    small adjustments

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -42,5 +42,27 @@ $(window).scroll(function() {
 });
 // Navbar Logo Code ------------------------------------------------------------- END
 
+  // Lightbox
+  $('.lightbox').click(function(){
+          $('this-page-img').prepend('<div class="backdrop"></div><div class="box"><div class="close">x</div></div>')
 
+          $('.backdrop, .box').animate({'opacity':'.50'}, 300, 'linear');
+          $('.box').animate({'opacity':'1.00'}, 300, 'linear');
+          $('.backdrop, .box').css('display', 'block');
+        });
+ 
+        $('.close').click(function(){
+          close_box();
+        });
+ 
+        $('.backdrop').click(function(){
+          close_box();
+        });
+
+        function close_box() //Closing Lightbox
+      {
+        $('.backdrop, .box').animate({'opacity':'0'}, 300, 'linear', function(){
+          $('.backdrop, .box').css('display', 'none');
+        });
+      }
 }); //document.ready
